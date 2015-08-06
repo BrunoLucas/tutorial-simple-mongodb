665880ef3e43431ba7ef8efe835453cd0d9324eb
Ballem <romarcio@gmail.com>
2015-08-06 18:41:54 -0300

    Classes de entidades.

---
+++ src/main/java/com/mballem/simplemongodb/entity/Person.java
@@ -0,0 +1,80 @@
+package com.mballem.simplemongodb.entity;
+
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Marcio Ballem
+ * Date: 09/07/12
+ * Time: 14:47
+ * http://mballem.com/
+ */
+public class Person implements Serializable {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private Phone phone;
+
+    public Person() {
+        super();
+    }
+
+    public Person(String firstName, String lastName, int age, Phone phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.phone = phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Phone getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Phone phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", phone=" + phone +
+                '}';
+    }
+}

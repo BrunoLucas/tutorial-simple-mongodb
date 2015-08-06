5aa97d9bedf6fa7f45abac866e9769b69eb2f7e2
Ballem <romarcio@gmail.com>
2015-08-06 18:41:23 -0300

    Classe de conexão do Java com o MongoDB

---
+++ src/main/java/com/mballem/simplemongodb/dao/MongoConnection.java
@@ -0,0 +1,52 @@
+package com.mballem.simplemongodb.dao;
+
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+
+import java.net.UnknownHostException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Marcio Ballem
+ * Date: 09/07/12
+ * Time: 14:55
+ * http://mballem.com/
+ */
+public class MongoConnection {
+
+    private static final String HOST = "localhost";
+    private static final int PORT = 27017;
+    private static final String DB_NAME = "simple-mongodb";
+
+    private static MongoConnection uniqInstance;
+    private static int mongoInstance = 1;
+
+    private Mongo mongo;
+    private DB db;
+
+    private MongoConnection() {
+        //construtor privado
+    }
+
+    //garante sempre uma unica instancia
+    public static synchronized MongoConnection getInstance() {
+        if (uniqInstance == null) {
+            uniqInstance = new MongoConnection();
+        }
+        return uniqInstance;
+    }
+
+    //garante um unico objeto mongo
+    public DB getDB() {
+        if (mongo == null) {
+            try {
+                mongo = new Mongo(HOST, PORT);
+                db = mongo.getDB(DB_NAME);
+                System.out.println("Mongo instance equals :> " + mongoInstance++);
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
+        }
+        return db;
+    }
+}
